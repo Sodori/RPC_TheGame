d586a4d0fe389490e454d8d1ecfc8dd9acec95f4
Sodori <jsodori@gmail.com>
2021-09-25 09:43:56 +0000

    more tweaking

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,8 +25,63 @@ document.addEventListener("DOMContentLoaded", function() {
     
 })
 
-function runGame (userChoice) {
-    let pcChoice = Math.floor(Math.random() * (1 - 3 + 1)) + 3;
+function runGame (userChoiceNum) {
+    let options = ['far fa-hand-rock fa-2x', 'far fa-hand-paper fa-2x', 'far fa-hand-scissors fa-2x']
 
+    let pcChoiceNum = Math.floor(Math.random() * 3) + 1;
+    let pcChoice = options[pcChoiceNum -1]
+
+    let userChoice = options[userChoiceNum -1]
+    
+    console.log(`Computer chose ${pcChoiceNum} and the player ${userChoiceNum}`);
     console.log(`Computer chose ${pcChoice} and the player ${userChoice}`);
+
+    document.getElementById("cValue").className = pcChoice;
+    document.getElementById("cValue").textContent = "\nComputer";
+    document.getElementById("cChoise").style.display = "initial";
+
+    switch (userChoiceNum) {
+        case 1:
+            switch (pcChoiceNum) {
+                case 1:
+                    score(3)
+                case 2:
+                    score(2)
+                case 3:
+                    score(1)
+            }
+        case 2:
+            switch (pcChoiceNum) {
+                case 1:
+                    score(3)
+                case 2:
+                    score(2)
+                case 3:
+                    score(1)
+            }
+        case 3:
+            switch (pcChoiceNum) {
+                case 1:
+                    score(3)
+                case 2:
+                    score(2)
+                case 3:
+                    score(1)
+            }
+    }
+}
+
+function score (result) {
+    let rValue
+    switch (score) {
+        case 1:
+            rValue = document.getElementsByTagName("sWin")
+            console.log(rValue);
+        case 2:
+            rValue = document.getElementsByTagName("sLoss")
+            console.log(rValue);
+        case 0:
+            rValue = document.getElementsByTagName("sTie")
+            console.log(rValue);
+    }
 }
