246740dff21b4a82469f2b645822f0d63bcab342
Sodori <jsodori@gmail.com>
2021-09-25 09:43:45 +0000

    basic structure done!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
-function getRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            let buttonName = this.getAttribute("class")
+            console.log(`You clicked ${buttonName}`)
+
+            switch (buttonName) {
+                case "rock":
+                    runGame(1);
+                    break;
+                case "paper":
+                    runGame(2);
+                    break;
+                case "scissor":
+                    runGame(3);
+                    break;
+                default:
+                    alert(`Unknown button;${buttonName}, what did you do?!`)
+                    throw `Can't understand ${buttonName}, aborting!`
+            }
+        })
+    }
+    
+})
+
+function runGame (userChoice) {
+    let pcChoice = Math.floor(Math.random() * (1 - 3 + 1)) + 3;
+
+    console.log(`Computer chose ${pcChoice} and the player ${userChoice}`);
 }
-
-let cChoise;
-let options = ['<i class="far fa-hand-rock"></i>', '<i class="far fa-hand-paper"></i>', '<i class="far fa-hand-scissors"></i>']
-
-console.log(getRandom(1, 10))
