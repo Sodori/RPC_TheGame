ab4354bbb4ed0b889165859be96d9b9d1ffa61fd
Sodori <jsodori@gmail.com>
2021-09-26 16:55:19 +0000

    Script complete and fully operational

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
+let cOpacity
+let cVisible
+let opacity
+
 document.addEventListener("DOMContentLoaded", function() {
+
+    let cOpacity
+    let cVisible
+
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons){
@@ -16,14 +24,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 case "scissor":
                     runGame(3);
                     break;
+                case "bReset":
+                    console.log("No more scores for you!");
+                    document.getElementById("sWin").textContent = "0"
+                    document.getElementById("sLoss").textContent = "0"
+                    document.getElementById("sTie").textContent = "0"
+                    document.getElementById("tAnnounce").style.opacity = 0
+                    document.getElementById("cValue").className = "fas fa-hands-wash fa-2x"
+                    setTimeout(SparklyClean, 2000)
+                    break;
                 default:
-                    alert(`Unknown button;${buttonName}, what did you do?!`)
-                    throw `Can't understand ${buttonName}, aborting!`
+                    alert(`Unknown button; ${buttonName}, what did you do?!`);
+                    throw `Can't understand ${buttonName}, aborting!`;
             }
         })
     }
     
 })
+
 
 function runGame (userChoiceNum) {
     let options = ['far fa-hand-rock fa-2x', 'far fa-hand-paper fa-2x', 'far fa-hand-scissors fa-2x']
@@ -32,56 +50,78 @@ function runGame (userChoiceNum) {
     let pcChoice = options[pcChoiceNum -1]
 
     let userChoice = options[userChoiceNum -1]
-    
-    console.log(`Computer chose ${pcChoiceNum} and the player ${userChoiceNum}`);
-    console.log(`Computer chose ${pcChoice} and the player ${userChoice}`);
 
     document.getElementById("cValue").className = pcChoice;
-    document.getElementById("cValue").textContent = "\nComputer";
-    document.getElementById("cChoise").style.display = "initial";
+    document.getElementById("cValue").style.opacity = "1";
 
-    switch (userChoiceNum) {
-        case 1:
-            switch (pcChoiceNum) {
-                case 1:
-                    score(3)
-                case 2:
-                    score(2)
-                case 3:
-                    score(1)
-            }
-        case 2:
-            switch (pcChoiceNum) {
-                case 1:
-                    score(3)
-                case 2:
-                    score(2)
-                case 3:
-                    score(1)
-            }
-        case 3:
-            switch (pcChoiceNum) {
-                case 1:
-                    score(3)
-                case 2:
-                    score(2)
-                case 3:
-                    score(1)
-            }
+    if (pcChoiceNum === userChoiceNum) {
+        console.log("Tie");
+        score(3)
+    } else if (userChoiceNum === 1 & pcChoiceNum === 2) {
+        console.log(`Lost! ${userChoiceNum} beats ${pcChoiceNum}`);
+        score(2)
+    } else if (userChoiceNum === 2 & pcChoiceNum === 3) {
+        console.log(`Lost! ${userChoiceNum} beats ${pcChoiceNum}`);
+        score(2)
+    } else if (userChoiceNum === 3 & pcChoiceNum === 1) {
+        console.log(`Lost! ${userChoiceNum} beats ${pcChoiceNum}`);
+        score(2)
+    } else {
+        console.log(`Victory! ${userChoiceNum} beats ${pcChoiceNum}`);
+        score(1)
     }
 }
 
 function score (result) {
     let rValue
-    switch (score) {
+    switch (result) {
         case 1:
-            rValue = document.getElementsByTagName("sWin")
-            console.log(rValue);
+            rValue = document.getElementById("sWin").textContent
+            console.log("Value is " + rValue);
+            document.getElementById("sWin").textContent = ++rValue
+            console.log(`New value is: ${rValue}`);
+            
+            ResultEffect("Winner!", "DarkOliveGreen")
+            return;
         case 2:
-            rValue = document.getElementsByTagName("sLoss")
-            console.log(rValue);
-        case 0:
-            rValue = document.getElementsByTagName("sTie")
-            console.log(rValue);
+            rValue = document.getElementById("sLoss").textContent
+            console.log("Value is " + rValue);
+            document.getElementById("sLoss").textContent = ++rValue
+            console.log(`New value is: ${rValue}`);
+            
+            ResultEffect("Looser!", "FireBrick")
+            return;
+        case 3:
+            rValue = document.getElementById("sTie").textContent
+            console.log("Value is " + rValue);
+            document.getElementById("sTie").textContent = ++rValue
+            console.log(`New value is: ${rValue}`);
+
+            ResultEffect("Draw!", "Gold")
+            return;
     }
+}
+
+function ResultEffect(ResultEffect, tColor) {
+    document.getElementById("tAnnounce").textContent = ResultEffect
+    document.getElementById("tAnnounce").style.color = tColor
+
+    document.getElementById("tAnnounce").style.opacity = "1"
+    clearTimeout(cVisible)
+    clearInterval(cOpacity)
+    cVisible = setTimeout(StayVisible, 2000)    
+}
+function StayVisible() {
+    cOpacity = setInterval(FadeOut, 50);
+    return;
+}
+
+function FadeOut() {
+    opacity = document.getElementById("tAnnounce").style.opacity - 0.025
+    document.getElementById("tAnnounce").style.opacity = opacity
+    if (opacity <= 0) {clearInterval(cOpacity)}
+}
+
+function SparklyClean() {
+    document.getElementById("cValue").className = "fas fa-hand-sparkles fa-2x"
 }
